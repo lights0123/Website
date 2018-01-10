55f49f3f41b61f8cd5ea903f20f2a10a6cfb1bf4
rshanedev <ryansshane@rocketmail.com>
2018-01-10 16:00:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ All rights reserved.
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <title>ActumCoin's Official Website is coming soon!</title>
-        <link rel="shortcut icon" href="logo-c.ico" />
+        <link rel="shortcut icon" href="logo.ico" />
         <!-- Bootstrap CDN -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
         <!-- Custom CSS -->
