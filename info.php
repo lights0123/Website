b36bba5f5522bec1ae4e8c4b44400ce89b999859
rshanedev <ryansshane@rocketmail.com>
2018-01-23 16:27:25 -0500

    Add files via upload

---
+++ info.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+Copyright (c) 2017 Ryan Shane
+All rights reserved.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>Actum's Official Website - Info</title>
+        <link rel="shortcut icon" href="logo.ico" />
+        <!-- Bootstrap CDN -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body class="bg">
+    <body>
+        <div class="jumbotron jumbotron-fluid padding-so-it-looks-good center" id="learn">
+            <h1 class="display-4 pad-bottom">Learn More About Actum</h1>
+            <div class="container">
+                <h2 class="display-5 center">What is Actum?</h2>
+                <p class="lead">Actum is a private blockchain system, where you can transfer digital tokens and cryptocurrencies.</p>
+                <h2 class="display-5 center">What is ActumCoin?</h2>
+                <p class="lead">ActumCoin is the native currency of Actum. It is used for most monetary transactions, although there may be more cryptocurrencies on Actum in the future.</p>
+                <h2 class="display-5 center">Why use Actum or ActumCoin?</h2>
+                <p class="lead">The primary reason is that you cannot get cheated or scammed because Actum uses <a href="https://www.coindesk.com/making-sense-smart-contracts/">smart contracts</a>, which automatically conduct transactions. Another reason is that cryptocurrencies are anonymous and secure, which allows for more security.</p>
+                <h2 class="display-5 center">What is ActumMiner?</h2>
+                <p class="lead">ActumMiner is a program for your computer that helps to verify transactions on the Actum blockchain. In exchange for the computing power of your computer, you are given ActumCoin. mines transactions of all tokens and currencies on Actum, but payouts are in ActumCoin.</p>
+                <h2 class="display-5 center">How does a blockchain work?</h2>
+                <p class="lead">A blockchain is a system that uses many "nodes" that store a record of every transaction. Each of these "nodes" stores a record of every transaction, a decentralized and secure system is possible.</p>
+                <h2 class="display-6 center">See more:</h2>
+                <p class="lead">Read the Actum whitepaper <a href="whitepaper">here</a>.</p>
+                
+            </div>
+        </div>
+    </body>
+</html>
