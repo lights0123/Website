2edc62c7b0169fa6757a9e46f6d6de0b8acc3190
rshanedev <ryansshane@rocketmail.com>
2018-01-27 10:10:03 -0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>Actum - Page Not Found</title>
+        <link rel="shortcut icon" href="http://actumcrypto.org/logo.ico" />
+        <!-- Bootstrap CDN -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="http://actumcrypto.org/css/main.css">
+    </head>
+    <body class="bg">
+        <div class="jumbotron jumbotron-fluid padding-so-it-looks-good-no-bottom-but-top center">
+            <a href="http://actumcrypto.org"><img src="http://actumcrypto.org/logo.png"></a>
+            <h1 class="display-4 bigger">404!</h1>
+        </div>
+        <div class="jumbotron jumbotron-fluid padding-so-it-looks-good-no-bottom center" id="pay">
+            <h1 class="display-4">Looks like we messed up!</h1
+            <div class="container">
+                <h2 class="display-5 center">The link to this page may be old or broken. If this problem persists, contact us at <a href="mailto:admin@actumcrypto.org">admin@actumcrypto.org</a>. Click <a href="/">here</a> to return home.</h2>
+            </div>
+        </div>
+
+        <!-- JQuery CDN -->
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    </body>
+</html>
