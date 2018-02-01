a854708eb140ad039bde621096f2e0f5d2c75206
rshanedev <ryansshane@rocketmail.com>
2018-01-31 19:50:20 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,26 +4,56 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Actum - Page Not Found</title>
-        <link rel="shortcut icon" href="http://actumcrypto.org/logo.ico" />
+        <title>ActumCrypto - Page Not Found</title>
+        <link rel="shortcut icon" href="logo.ico" />
         <!-- Bootstrap CDN -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
         <!-- Custom CSS -->
-        <link rel="stylesheet" href="http://actumcrypto.org/css/main.css">
+        <link rel="stylesheet" href="css/main.css">
     </head>
-    <body class="bg">
-        <div class="jumbotron jumbotron-fluid padding-so-it-looks-good-no-bottom-but-top center">
-            <a href="http://actumcrypto.org"><img src="http://actumcrypto.org/logo.png"></a>
-            <h1 class="display-4 bigger upsidedown">404!</h1>
+    <body class="bg-img">
+        <button class="nav-collapse" onclick="reveal();">
+            <!-- material icon menu -->
+            <img src="svg/nav.svg">
+        </button>
+        <ul class="nav hidden nav-nice">
+            <li class="nav-item">
+                <a class="nav-link" href="./">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./">FAQ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./downloads">Downloads</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./develop">Develop an Actum App</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="./tokens">List of Actum Tokens</a>
+            </li>
+        </ul>
+        <div class="jumbotron jumbotron-fluid pad pad-top wid">
+            <img src="logo.png">
+            <h1 class="display-4 bigger"><b>404!!</b></h1>
         </div>
-        <div class="jumbotron jumbotron-fluid padding-so-it-looks-good-no-bottom center" id="pay">
-            <h1 class="display-4">Looks like we messed up!</h1
-            <div class="container">
-                <h2 class="display-5 center">The link to this page may be old or broken. If this problem persists, contact us at <a href="mailto:admin@actumcrypto.org">admin@actumcrypto.org</a>. Click <a href="/">here</a> to return home.</h2>
-            </div>
+        <div class="jumbotron jumbotron-fluid pad wid" id="pay">
+            <h1 class="display-4">Looks like we messed up!</h1>
+            <h2 class="display-5">The link to this page may be old or broken. If this problem persists, contact us at <a href="mailto:admin@actumcrypto.org">admin@actumcrypto.org</a>. Click <a href="/">here</a> to return home.</h2>
         </div>
 
         <!-- JQuery CDN -->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>
+            var hidden = true;
+            function reveal() {
+                if (hidden) {
+                    $(".hidden").attr("class", "nav reveal nav-nice");
+                } else {
+                    $(".reveal").attr("class", "nav hidden nav-nice");
+                }
+                hidden = !hidden;
+            }
+        </script>
     </body>
 </html>
