3675a4d2f5f6fefa17de68af51387788bbc30e68
rshanedev <ryansshane@rocketmail.com>
2018-01-31 19:56:29 -0500

    Create tokens.php

---
+++ apply/tokens.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>ActumCrypto - Apply to Create a Token</title>
+        <link rel="shortcut icon" href="../logo.ico" />
+        <!-- Bootstrap CDN -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="../css/main.css">
+    </head>
+    <body class="bg-img">
+        <button class="nav-collapse" onclick="reveal();">
+            <!-- material icon menu -->
+            <img src="../svg/nav.svg">
+        </button>
+        <ul class="nav hidden nav-nice">
+            <li class="nav-item">
+                <a class="nav-link" href="../">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../faq">FAQ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../downloads">Downloads</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../develop">Develop an Actum App</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../tokens">List of Actum Tokens</a>
+            </li>
+        </ul>
+        <div class="jumbotron jumbotron-fluid pad pad-top wid">
+            <img src="../logo.png">
+            <h1 class="display-4 bigger"><b>ActumCrypto</b></h1>
+            <?php
+            if (isset($_GET['c'])) {
+                if ($_GET['c'] == "1") {
+                    echo '<div class="alert alert-success" role="alert">Your application has been sent successfully!</div>';
+                }
+            }
+            ?>
+            <form action="./submit" method="post">
+                <div class="form-group">
+                    <label for="n">Organization Name*</label>
+                    <input type="text" class="form-control" id="n" placeholder="Enter organization name" required>
+                    <small class="form-text text-muted">If you are an individual, enter your name.</small>
+                </div>
+                <div class="form-group">
+                    <label for="e">Email Address*</label>
+                    <input type="email" class="form-control" id="e" placeholder="Enter email" required>
+                </div>
+                <div class="form-group">
+                    <label for="tn">Proposed Token Name*</label>
+                    <input type="text" class="form-control" id="tn" placeholder="Enter proposed token name" required>
+                </div>
+                <div class="form-group">
+                    <label for="ts">Proposed Token Symbol*</label>
+                    <input type="text" class="form-control" id="ts" placeholder="Enter proposed token symbol" required>
+                    <small class="form-text text-muted">Three letters that act as a "symbol" for your token.</small>
+                </div>
+                <div class="form-group">
+                    <label for="ta">Proposed Initial Amount*</label>
+                    <input type="text" class="form-control" id="ta" placeholder="Enter proposed token inital amount" required>
+                </div>
+                <div class="form-group">
+                    <label for="tv">Proposed Initial Value (in ActumCoin)*</label>
+                    <input type="text" class="form-control" id="tv" placeholder="Enter proposed token inital value" required>
+                </div>
+                <div class="form-group">
+                    <label for="c">Comments</label>
+                    <textarea class="form-control" id="c" placeholder="Enter any comments"></textarea>
+                </div>
+                <small class="form-text text-muted">*These fields are required</small>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <!-- JQuery CDN -->
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>
+            var hidden = true;
+            function reveal() {
+                if (hidden) {
+                    $(".hidden").attr("class", "nav reveal nav-nice");
+                } else {
+                    $(".reveal").attr("class", "nav hidden nav-nice");
+                }
+                hidden = !hidden;
+            }
+        </script>
+    </body>
+</html>
