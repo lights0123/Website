53d8dfd3ddc506b4c55f219cf183894521277e76
rshanedev <ryansshane@rocketmail.com>
2018-01-31 19:53:56 -0500

    Update tokens.php

--- tokens.php
+++ tokens.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Actum - Tokens</title>
+        <title>ActumCrypto - List of Actum Tokens</title>
         <link rel="shortcut icon" href="logo.ico" />
         <!-- Bootstrap CDN -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
@@ -33,12 +33,14 @@
                 <a class="nav-link active" href="">List of Actum Tokens</a>
             </li>
         </ul>
-        <div class="jumbotron jumbotron-fluid pad pad-top">
+        <div class="jumbotron jumbotron-fluid pad pad-top wid">
             <img src="logo.png">
             <h1 class="display-4 bigger"><b>Actum Tokens</b></h1>
         </div>
-        <div class="jumbotron jumbotron-fluid pad">
-            <table class="table wid">
+        <div class="jumbotron jumbotron-fluid pad wid">
+            <p class="lead">View the steps to create an Actum token <a href="./develop/create">here</a>.</p>
+            <p class="lead">Apply to get on this list <a href="./apply/tokens">here</a>.</p>
+            <table class="table">
                 <thead class="thead-dark">
                     <tr>
                         <th scope="col">Symbol</th>
