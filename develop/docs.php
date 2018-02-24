c0afb6c82e2a9859f2698bdf1d9756f037496f35
rshanedev <ryansshane@rocketmail.com>
2018-02-23 19:43:51 -0500

    Update docs.php

--- develop/docs.php
+++ develop/docs.php
@@ -31,9 +31,13 @@
             </li>
         </ul>
         <div class="jumbotron jumbotron-fluid pad pad-top">
-            <img src="../logo.png">
+            <img src="../svg/logo.svg">
             <h1 class="display-4 bigger"><b>Coming Soon!</b></h1>
         </div>
+        <footer class="footer jumbotron jumbotron-fluid pad wid">
+            <p class="lead">Join our official <a href="http://reddit.com/r/actumcrypto">subreddit</a>!</p>
+            <p class="lead">Join our <a href="https://discord.gg/ybzTrHS">Discord server</a>!</p>
+        </footer>
         <!-- JQuery CDN -->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>
