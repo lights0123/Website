b8ebaf82823b55415ee2030de6bbdd51d4d03d53
rshanedev <ryansshane@rocketmail.com>
2018-02-23 19:42:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,11 +10,23 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
         <!-- Custom CSS -->
         <link rel="stylesheet" href="css/main.css">
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113481337-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-113481337-1');
+        </script>
     </head>
     <body class="bg-img">
         <button class="nav-collapse" onclick="reveal();">
             <!-- material icon menu -->
-            <img src="svg/nav.svg">
+            <img src="./svg/nav.svg">
         </button>
         <ul class="nav hidden nav-nice">
             <li class="nav-item">
@@ -31,7 +43,7 @@
             </li>
         </ul>
         <div class="jumbotron jumbotron-fluid pad pad-top wid">
-            <img src="logo.png">
+            <img src="./svg/logo.svg">
             <h1 class="display-4 bigger"><b>ActumCrypto</b></h1>
             <p class="lead">ActumCrypto is a blockchain and system of smart contracts. These are programmed to preform transactions, and cannot be interfered with. This means that Actum smart contracts are free from fraud and unexpected changes. With your own Actum token and smart contracts, you can create a rewards program, a cryptocurrency, a market, or anything else you can think of!</p>
             <p class="lead">A blockchain is free from downtime and slow regulatory processes, so Actum tokens transfer are fast and reliable. Actum apps can also use ActumCoin, the native cryptocurrency of Actum as a payment method.</p>
@@ -55,6 +67,33 @@
             <p class="lead">Since the ActumCrypto system uses a modified version of round-robin mining, all miners are granted a fair chance at mining a block, and earning ActumCoin</p>
             <a class="btn btn btn-dark" href="./downloads#miner">Download ActumMiner!</a>
         </div>
+        <div class="jumbotron jumbotron-fluid pad wid">
+            <h1 class="display-4">News</h1>
+            <?php
+            include "./db.php";
+            
+            $sql = "SELECT * FROM news WHERE id=(SELECT max(id) FROM news)";
+    
+    $result = mysqli_query($link, $sql);
+            
+    $row = mysqli_fetch_row($result);
+            ?>
+            <a href="./article?a=<?= $row[0] ?>"><h2 class="display-5"><?= $row[1]?></h2></a>
+            <small class="form-text text-muted"><?= $row[2] ?></small>
+            <p class="lead"> <?php 
+                $pos=strpos($row[3],' ',100);
+                $a = substr($row[3],0,$pos );
+                if ($a == $row[3]) {
+                	echo $a;
+                } else {
+                	echo $a . '... <a href="./article?a=' . $row[0] . '">Read More</a>';
+                } ?> </p>
+            <p class="lead">This is displaying a preview of the newest news article. Click <a href="./news">here</a> to see more articles. </p>
+        </div>
+        <footer class="footer jumbotron jumbotron-fluid pad wid">
+            <p class="lead">Join our official <a href="http://reddit.com/r/actumcrypto">subreddit</a>!</p>
+            <p class="lead">Join our <a href="https://discord.gg/ybzTrHS">Discord server</a>!</p>
+        </footer>
         <!-- JQuery CDN -->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>
