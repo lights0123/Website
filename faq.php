d6fb9ba933cefbfdb5fc7b5ead31c53a1972a2bd
rshanedev <ryansshane@rocketmail.com>
2018-02-23 19:46:58 -0500

    Update faq.php

--- faq.php
+++ faq.php
@@ -31,14 +31,14 @@
             </li>
         </ul>
         <div class="jumbotron jumbotron-fluid pad pad-top wid">
-            <img src="logo.png">
+            <img src="./svg/logo.svg">
             <h1 class="display-4 bigger"><b>FAQ</b></h1>
         </div>
         <div class="jumbotron jumbotron-fluid pad wid">
             <h2 class="display-5">What is ActumCrypto?</h2>
             <p class="lead">ActumCrypto is a private blockchain system, where you can transfer digital tokens and cryptocurrencies.</p>
             <h2 class="display-5">What is Actum?</h2>
-            <p class="lead">Actum is another name used to refer to certain parts of the ActumCrypto system. For example, they are Actum tokens, but they areon the ActumCrypto system.</p>
+            <p class="lead">Actum is another name used to refer to certain parts of the ActumCrypto blockchain. For example, they are Actum tokens, but they are on the ActumCrypto blockchain.</p>
             <h2 class="display-5">Why the name Actum?</h2>
             <p class="lead">Actum is a Latin word meaning "transaction."</p>
             <h2 class="display-5">What is ActumCoin?</h2>
@@ -53,6 +53,10 @@
             <p class="lead">Read the Actum whitepaper <a href="./whitepaper">here</a>.</p>
             <p class="lead">For press inquires email us at <a href="mailto:info@actumcrypto.org">info@actumcrypto.org</a>.</p>
         </div>
+        <footer class="footer jumbotron jumbotron-fluid pad wid">
+            <p class="lead">Join our official <a href="http://reddit.com/r/actumcrypto">subreddit</a>!</p>
+            <p class="lead">Join our <a href="https://discord.gg/ybzTrHS">Discord server</a>!</p>
+        </footer>
         <!-- JQuery CDN -->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>
