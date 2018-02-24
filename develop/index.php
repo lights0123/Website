3d593fd662b62ddba7fd8e3829098213b00e36aa
rshanedev <ryansshane@rocketmail.com>
2018-02-23 19:44:09 -0500

    Update index.php

--- develop/index.php
+++ develop/index.php
@@ -31,7 +31,7 @@
             </li>
         </ul>
         <div class="jumbotron jumbotron-fluid pad pad-top wid">
-            <img src="../logo.png">
+            <img src="../svg/logo.svg">
             <h1 class="display-4 bigger"><b>Develop an Actum App</b></h1>
         </div>
         <div class="jumbotron jumbotron-fluid pad wid">
@@ -44,6 +44,10 @@
             <p class="lead">Smart contracts can be programmed with conditional statements that rely on outside factors to be satisfied. This means that you can create an app that automatically pays workers for completing tasks, reward customers for shopping at your store, pay out prizes for winning a game, or anything else you can think of!</p>
             <a class="btn btn btn-dark" href="./create">Create an Actum App!</a>
         </div>
+        <footer class="footer jumbotron jumbotron-fluid pad wid">
+            <p class="lead">Join our official <a href="http://reddit.com/r/actumcrypto">subreddit</a>!</p>
+            <p class="lead">Join our <a href="https://discord.gg/ybzTrHS">Discord server</a>!</p>
+        </footer>
         <!-- JQuery CDN -->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>
