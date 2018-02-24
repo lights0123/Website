2e45c57bc91e91446f2b670000dabf129675175a
rshanedev <ryansshane@rocketmail.com>
2018-02-23 19:47:50 -0500

    Update vendors.php

--- vendors.php
+++ vendors.php
@@ -31,7 +31,7 @@
             </li>
         </ul>
         <div class="jumbotron jumbotron-fluid pad pad-top wid">
-            <img src="logo.png">
+            <img src="./svg/logo.svg">
             <h1 class="display-4 bigger"><b>Vendors That Accept ActumCoin</b></h1>
         </div>
         <div class="jumbotron jumbotron-fluid pad wid">
@@ -53,6 +53,10 @@
                 </tbody>
             </table>
         </div>
+        <footer class="footer jumbotron jumbotron-fluid pad wid">
+            <p class="lead">Join our official <a href="http://reddit.com/r/actumcrypto">subreddit</a>!</p>
+            <p class="lead">Join our <a href="https://discord.gg/ybzTrHS">Discord server</a>!</p>
+        </footer>
         <!-- JQuery CDN -->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>
